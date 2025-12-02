97d3294a314995784d351e75d98758d7313de50b
mmarofin <marofinus@gmail.com>
2025-12-02 10:44:19 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,5 +3,14 @@ maro = {}
 -- If set to true, the radial menu will be shown when the player holds the 'radial' key.
 maro.showradial = true
 
-maro.commands {
+maro.commands = {
     alert = "libalert"
+    context = "libcontext"
+    dialog = "libdialog"
+    menu = "menu"
+    noti = "libnotify"
+    prog = "libprog"
+    progcircle = "libprogcircle"
+    skill = "libskillcheck"
+    textui = "libtextui"
+}
